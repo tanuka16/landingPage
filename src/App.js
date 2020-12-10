c3daf6fe9ab28b6be1fd00cc83233170f9370250
Tanuka16 <tanuka.das12@gmail.com>
2020-12-10 14:52:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import LandingIndex from './containers/LandingIndex'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingIndex/>
+    </div>
+  );
+}
+
+export default App;
